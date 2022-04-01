7e72188c0093a3b1f4a38b50f889df9a98aebfee
gorbulevsv <gorbulevsv@yandex.ru>
2022-04-02 00:14:47 +0300

    1

--- app/src/main/java/com/example/androidcsi/fragments/highRatingFragment.java
+++ app/src/main/java/com/example/androidcsi/fragments/highRatingFragment.java
@@ -7,14 +7,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
+import com.example.androidcsi.MainActivity;
 import com.example.androidcsi.R;
 
 public class highRatingFragment extends Fragment {
+    View view;
+    NumberPicker pickerLowRating;
+    NumberPicker pickerCSI;
+    TextView textResult;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_high_rating, container, false);
+        view = inflater.inflate(R.layout.fragment_high_rating, container, false);
+
+        pickerLowRating = (NumberPicker) view.findViewById(R.id.picker_low_rating);
+        pickerLowRating.setMinValue(0);
+        pickerLowRating.setMaxValue(100);
+        pickerCSI = (NumberPicker) view.findViewById(R.id.picker_CSI);
+        pickerCSI.setMinValue(0);
+        pickerCSI.setMaxValue(99);
+
+        pickerLowRating.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                textResult = view.findViewById(R.id.textResult);
+                textResult.setText(Integer.toString(getResult(pickerLowRating.getValue(), pickerCSI.getValue())));
+            }
+        });
+
+        pickerCSI.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                textResult = view.findViewById(R.id.textResult);
+                textResult.setText(Integer.toString(getResult(pickerLowRating.getValue(), pickerCSI.getValue())));
+            }
+        });
+
+        return view;
+    }
+
+    public int getResult(int LowRating, int CSI) {
+        return (LowRating * (100 + CSI)) / (100 - CSI);
     }
 }
