aaecb60ee7c032ba207a5987f12f3d2a5afc17e9
gorbulevsv <gorbulevsv@yandex.ru>
2022-04-08 08:59:11 +0300

    1

--- app/mylibrary/src/main/java/com/example/mylibrary/DialogFragment.java
+++ app/mylibrary/src/main/java/com/example/mylibrary/DialogFragment.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,6 +29,21 @@ public class DialogFragment extends AppCompatDialogFragment {
         EditText editText = view.findViewById(R.id.editText);
         editText.setText(((TextView) preference.findViewById(R.id.textValue)).getText().toString().isEmpty() ? "0" : ((TextView) preference.findViewById(R.id.textValue)).getText().toString());
         editText.requestFocus();
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence chrSequence, int i, int i1, int i2) {
+                if(chrSequence.toString().isEmpty()) editText.setText("0");
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(((TextView)preference.findViewById(R.id.textTitle)).getText().toString().isEmpty() ? "Задать значение" : ((TextView)preference.findViewById(R.id.textTitle)).getText().toString())
